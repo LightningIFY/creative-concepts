c0f6ad3d1017608b4d9c080443a5fa85412b085e
Shaurya <agrawalshaurya93@gmail.com>
2026-06-07 21:13:56 +0530

    Luxury navbar redesign

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -72,7 +72,7 @@ export default function Hero() {
             fontWeight: 500,
             lineHeight: "1.15",
             color: "#c96384",
-            marginTop: "18px",
+            marginTop: "4px",
           }}
         >
           Style, Elegance & Grace
