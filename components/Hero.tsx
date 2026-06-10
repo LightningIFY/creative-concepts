eded9ef3d91e9b4fc775fd6da6d5bc81760a33b6
Shaurya <agrawalshaurya93@gmail.com>
2026-06-10 19:07:29 +0530

    Luxury navbar redesign

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage: "url('/portfolio/6.jpg')",
+          backgroundImage: "url('/portfolio/6.jfif')",
         }}
       />
 
